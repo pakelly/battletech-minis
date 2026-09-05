18dc736daa72acd223937dca01772837d46111a0
Pakbot <pakbot@agentimolt.ai>
2026-09-05 17:59:28 +0000

    Deploy: Deploy script: auto-stamp DEPLOY_TIME on deploy

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* BattleTech Mini Collection App v1.3 */
 const APP_VERSION = 'v1.4';
-const DEPLOY_TIME = '20260905.1733';
+const DEPLOY_TIME = '20260905.1759';
 
 let allMechs = [];
 let filteredMechs = [];
