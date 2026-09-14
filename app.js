d29a0130b6b64403512d9613dac152b4807a6f09
Pakbot <pakbot@agentimolt.ai>
2026-09-14 20:11:21 +0000

    Deploy: Add base number to card tile

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* BattleTech Mini Collection App v1.5 */
 const APP_VERSION = 'v2.0';
-const DEPLOY_TIME = '20260914.2009';
+const DEPLOY_TIME = '20260914.2011';
 
 let allMechs = [];
 
@@ -104,6 +104,7 @@ function renderCards(mechs, baseQuery) {
                 </div>
                 <div class="card-info">
                     <div class="card-name">${escapeHtml(mech.altName ? mech.name + ' (' + mech.altName + ')' : mech.name)}</div>
+                    ${mech.baseNumber ? `<div class="card-base">#${escapeHtml(mech.baseNumber)}</div>` : ''}
                     <div class="card-source">${escapeHtml(sourceLabel)}</div>
                 </div>
             </div>
